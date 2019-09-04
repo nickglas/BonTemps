2a26edec8c168f6312b56eb7d137ad3f1a13684e
Nick Glas <nickglas@hotmail.nl>
2019-09-04 13:53:42 -0500

    Systeem area

--- BonTemps/Startup.cs
+++ BonTemps/Startup.cs
@@ -94,6 +94,9 @@ namespace BonTemps
                 app.UseMvc(routes =>
                 {
                     routes.MapRoute(
+                           name: "areas",
+                           template: "{area:exists}/{controller=ReserveerSysteem}/{action=Index}/{id?}");
+                    routes.MapRoute(
                         name: "default",
                         template: "{controller=Home}/{action=Index}/{id?}");
                 });
