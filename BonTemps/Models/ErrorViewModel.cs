3a877abea3ce8d28716834af534378f5069d12d3
Nick Glas <nickglas@hotmail.nl>
2019-09-04 10:12:57 +0200

    project

---
+++ BonTemps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BonTemps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
