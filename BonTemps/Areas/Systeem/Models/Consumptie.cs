4d59e1d934a43758fb82d1398cdfcd2a49f16d34
Nick Glas <nickglas@hotmail.nl>
2019-09-11 10:09:53 -0500

    consumpties , controller, view

--- BonTemps/Areas/Systeem/Models/Consumptie.cs
+++ BonTemps/Areas/Systeem/Models/Consumptie.cs
@@ -7,6 +7,12 @@ namespace BonTemps.Areas.Systeem.Models
 {
     public class Consumptie
     {
+        string Category_eten = "Eten";
+        string Category_Drinken = "Drinken";
+        string Category_Deserts = "Deserts";
+
+
+
         public int Id { get; set; }
         public string Naam { get; set; }
         public string Beschrijving { get; set; }
