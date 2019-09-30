b6661bf88666faa7eb669e7feb8722048455859c
Nick Glas <nickglas@hotmail.nl>
2019-09-30 14:34:08 +0200

    reserveringen, inkomende reserveringen

--- BonTemps/Migrations/20190930114159_goedkeuring.Designer.cs
+++ BonTemps/Migrations/20190930114159_goedkeuring.Designer.cs
@@ -4,14 +4,16 @@ using BonTemps.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BonTemps.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190930114159_goedkeuring")]
+    partial class goedkeuring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -163,8 +165,6 @@ namespace BonTemps.Migrations
                     b.Property<string>("MobielTelefoonNummer");
 
                     b.Property<string>("NaamReserveerder");
-
-                    b.Property<string>("Opmerking");
 
                     b.Property<DateTime>("ReserveringAangemaakt");
 
---
+++ BonTemps/Migrations/20190930114159_goedkeuring.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BonTemps.Migrations
+{
+    public partial class goedkeuring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Goedkeuring",
+                table: "Reserveringen",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Goedkeuring",
+                table: "Reserveringen");
+        }
+    }
+}
