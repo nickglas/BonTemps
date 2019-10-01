54a19ec0055f4a893d49f07210ccadb795a60fd8
Dennis <dvandermolen0@gmail.com>
2019-10-01 11:25:13 +0200

    Kliene css en index van chef veranderingen

--- BonTemps/Areas/Systeem/Models/Menu.cs
+++ BonTemps/Areas/Systeem/Models/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BonTemps.Areas.Systeem.Models
     public class Menu
     {
         public int Id { get; set; }
+        [Display(Name = "Menu naam")]
         public string Menu_naam { get; set; }
         public string Beschrijving { get; set; }
 
