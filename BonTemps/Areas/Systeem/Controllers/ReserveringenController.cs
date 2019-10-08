857816e0ef30625059faefbdc5d99dcdb8536b3f
Marco Boergonje <marcoboergonje@gmail.com>
2019-10-08 11:28:29 +0200

    alles in 1 pakket van reserveringen

--- BonTemps/Areas/Systeem/Controllers/ReserveringenController.cs
+++ BonTemps/Areas/Systeem/Controllers/ReserveringenController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BonTemps.Data;
+using BonTemps.Models;
 
 namespace BonTemps.Areas.Systeem.Models
 {
---
+++ BonTemps/Areas/Systeem/Models/ReserveringTafel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonTemps.Areas.Systeem.Models
+{
+    public class ReserveringTafel
+    {
+        public int ResId { get; set; }
+        public int Id { get; set; }
+    }
+}
