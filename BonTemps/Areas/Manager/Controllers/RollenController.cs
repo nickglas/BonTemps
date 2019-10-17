4fc4b06a42c7bcdba5279352a0876b0d18c38da5
Nick Glas <nickglas@hotmail.nl>
2019-10-17 15:54:31 +0200

    bestellingen en rollen fix

---
+++ BonTemps/Areas/Manager/Controllers/RollenController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BonTemps.Data;
+using BonTemps.Models;
+
+namespace BonTemps.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class RollenController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RollenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Manager/Rollen
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rol.ToListAsync());
+        }
+
+        // GET: Manager/Rollen/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rol
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // GET: Manager/Rollen/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manager/Rollen/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Beschrijving,Aanmaakdatum,Id,Name,NormalizedName,ConcurrencyStamp")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rol);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
+
+        // GET: Manager/Rollen/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rol.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Manager/Rollen/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Beschrijving,Aanmaakdatum,Id,Name,NormalizedName,ConcurrencyStamp")] Rol rol)
+        {
+            if (id != rol.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rol);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RolExists(rol.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
+
+        // GET: Manager/Rollen/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rol
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // POST: Manager/Rollen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var rol = await _context.Rol.FindAsync(id);
+            _context.Rol.Remove(rol);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RolExists(string id)
+        {
+            return _context.Rol.Any(e => e.Id == id);
+        }
+    }
+}
