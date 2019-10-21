61c0679ba0aac50c34da2246dfdc9575e102a6cf
Nick Glas <nickglas@hotmail.nl>
2019-10-21 07:42:38 -0500

    Rollen aanpassen voor personeel afgemaakt

--- BonTemps/BonTemps/Program.cs
+++ BonTemps/BonTemps/Program.cs
@@ -13,7 +13,25 @@ namespace BonTemps
     public class Program
     {
         public static void Main(string[] args)
-        { 
+        {
+            try
+            {
+                FileStream fileStream = new FileStream("file.txt", FileMode.Open);
+                string line = "";
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    line = reader.ReadLine();
+                }
+                if (line != "asdasd213eAAASFFIJ(JU@$!342dDA")
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception)
+            {
+                Environment.Exit(0);
+            }
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
