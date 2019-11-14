1d662c521bb48a15af7288301124ca3716b1b8ef
Nick Glas <nickglas@hotmail.nl>
2019-11-14 15:04:32 +0100

    hardcoded api call reservering maken

--- BonTemps/Controllers/APIController.cs
+++ BonTemps/Controllers/APIController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BonTemps.Areas.Systeem.Models;
 using BonTemps.Data;
+using BonTemps.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -22,14 +23,31 @@ namespace BonTemps.Controllers.API
 
 
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult GetConsumptie()
         {
-            Console.WriteLine("\n\n IETS !! \n\n");
             List<Consumptie> Cons = _context.Consumpties.ToList();
-
             var Content = JsonConvert.SerializeObject(Cons);
             return new JsonResult(new { TestObject = Cons });
         }
 
+        public async Task<IActionResult> MakeReservervation()
+        {
+            Reservering res = new Reservering()
+            {
+                NaamReserveerder = "test",
+                Email = "Test",
+                HuisTelefoonNummer = "123",
+                MobielTelefoonNummer = "123",
+                AantalPersonen = 1,
+                ReserveringAangemaakt = DateTime.Now,
+                ReserveringsDatum = DateTime.Now,
+                Goedkeuring = false,
+                tafelsId = _context.Tafels.Where(x => x.Bezet == false).FirstOrDefault().Id
+            };
+            await _context.Reserveringen.AddAsync(res);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
+        
 }
